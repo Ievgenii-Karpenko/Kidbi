8a97661cb9395fb399b845f5372758038349c16f
ievgenii.karpenko <gmen.karpenko@gmail.com>
2021-03-28 12:59:43 +0300

    Added kontrolna

---
+++ Controlna/Program.cs
@@ -0,0 +1,72 @@
+﻿// Andriy
+
+// Створити два списки, один для імен людей, інший для їх віку (відповідно типу string та int).
+// Запитати в користувача по черзі 5 імен записати ці значення в список імен (виконати це в циклі).
+// Запитати в користувача по черзі вік для кожного імені записати ці значення в список віку (виконати це в циклі).
+// Якщо введене число буде більше за 90 - згенерувати помилку.
+// 
+// Вивести на екран всі імена, які починаються на букву "V".
+// Записати в файл всі числа зі списку віку, які більше 15.
+// Розрахувати рік народження кожного з користувачів і також вивести роки народження на екран.
+
+// *Реалізувати введення вхідної інформації шляхом зчитування з файлів
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace kr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            List<int> ages = new List<int>();
+
+            Console.WriteLine("Enter 5 names");
+            for (int i = 0; i < 5; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            Console.WriteLine("Enter 5 ages");
+            for (int i = 0; i < 5; i++)
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age > 90)
+                {
+                    throw new Exception(" >90");
+                }
+                ages.Add(age);
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (names[i].StartsWith("V"))
+                {
+                    Console.WriteLine(names[i]);
+                }
+            }
+
+            string content = "";
+            for (int i = 0; i < ages.Count; i++)
+            {
+                if (ages[i] > 15)
+                {
+                    content += ages[i] + "\n";
+                }
+            }
+
+            File.WriteAllText(@"C:\test\ages.txt", content);
+
+            for (int i = 0; i < ages.Count; i++)
+            {
+                {
+                    int birthYear = 2021 - ages[i];
+                    Console.WriteLine("You were born in the year of" + birthYear);
+                }
+            }
+        }
+    }
+}
--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -10,36 +10,36 @@ namespace Lesson8
         {
             try 
             {
-                string path = @"C:\test\MyFile.txt";
-                string copyPath = @"C:\test\CopyMyFile.txt";
-                //File.Create(path);
-                string content = "My name is Ievgenii";
+                //string path = @"C:\test\MyFile.txt";
+                //string copyPath = @"C:\test\CopyMyFile.txt";
+                ////File.Create(path);
+                //string content = "My name is Ievgenii";
 
-                File.WriteAllText(path, content);
+                //File.WriteAllText(path, content);
 
-                if (!File.Exists(copyPath))  // !true = false; !false = true
-                {
-                    File.Copy(path, copyPath);
-                }
+                //if (!File.Exists(copyPath))  // !true = false; !false = true
+                //{
+                //    File.Copy(path, copyPath);
+                //}
 
-                if (File.Exists(@"C:\test\MyFile2.txt"))
-                {
-                    File.Delete(@"C:\test\MyFile2.txt");
-                }
+                //if (File.Exists(@"C:\test\MyFile2.txt"))
+                //{
+                //    File.Delete(@"C:\test\MyFile2.txt");
+                //}
 
-                File.Move(@"C:\test\lalala.txt", @"C:\lalala.txt");
+                //File.Move(@"C:\test\lalala.txt", @"C:\lalala.txt");
 
-                string[] data = File.ReadAllLines(@"C:\test\Data.txt");
+                //string[] data = File.ReadAllLines(@"C:\test\Data.txt");
 
-                string name = data[0];
-                string surname = data[1];
-                int age = Convert.ToInt32(data[2]);
+                //string name = data[0];
+                //string surname = data[1];
+                //int age = Convert.ToInt32(data[2]);
 
-                Console.WriteLine("My name is: " + name);
-                Console.WriteLine("My surname is: " + surname);
-                Console.WriteLine("My age is: " + age);
+                //Console.WriteLine("My name is: " + name);
+                //Console.WriteLine("My surname is: " + surname);
+                //Console.WriteLine("My age is: " + age);
 
-                string site = @"https://codeshare.io/5ZgBKe";
+                string site = @"https://dou.ua/";
                 string chrome = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
 
                 ProcessStartInfo proces = new ProcessStartInfo(chrome, site);
